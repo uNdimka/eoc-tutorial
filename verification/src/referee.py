bdf2e568771d0fe6da012e1e296415fc9337f4b8
Alexander Lyabah <a.lyabah@checkio.org>
2019-04-04 16:59:12 +0300

    hf

--- verification/src/referee.py
+++ verification/src/referee.py
@@ -2,31 +2,31 @@ from checkio_referee import RefereeRank, ENV_NAME
 
 
 
-    import settings_env
-    from tests import TESTS
+import settings_env
+from tests import TESTS
 
 
-    class Referee(RefereeRank):
-        TESTS = TESTS
-        ENVIRONMENTS = settings_env.ENVIRONMENTS
+class Referee(RefereeRank):
+    TESTS = TESTS
+    ENVIRONMENTS = settings_env.ENVIRONMENTS
 
-        DEFAULT_FUNCTION_NAME = "mult_two"
-        FUNCTION_NAMES = {
-            "python_3": "mult_two",
-            "js_node": "multTwo"
-        }
-        ENV_COVERCODE = {
-            ENV_NAME.PYTHON: '''
+    DEFAULT_FUNCTION_NAME = "mult_two"
+    FUNCTION_NAMES = {
+        "python_3": "mult_two",
+        "js_node": "multTwo"
+    }
+    ENV_COVERCODE = {
+        ENV_NAME.PYTHON: '''
 
-    def cover(func, in_data):
-        return func(*in_data)
+def cover(func, in_data):
+    return func(*in_data)
 
     ''',
             ENV_NAME.JS_NODE: '''
 
-    function cover(func, in_data) {
-        return func.apply(this, in_data)
-    }
+function cover(func, in_data) {
+    return func.apply(this, in_data)
+}
 
     '''
-        }
+    }
